a1003848e20ba3765248a2fd5486c0d59e4c9540
Ignacio Vargas <ignaevc@gmail.com>
2024-11-04 16:12:30 -0600

    Get rid of Original Uri <p> in the not found page

--- crates/web-server/src/routes/not_found.rs
+++ crates/web-server/src/routes/not_found.rs
@@ -1,18 +1,14 @@
-use axum::{extract::OriginalUri, http::StatusCode};
+use axum::http::StatusCode;
 use maud::{html, Markup};
 
 use super::extractors::Layout;
 
 #[cfg_attr(feature = "debug", axum::debug_handler)]
-pub async fn not_found(
-    layout: Layout,
-    OriginalUri(original_uri): OriginalUri,
-) -> (StatusCode, Markup) {
+pub async fn not_found(layout: Layout) -> (StatusCode, Markup) {
     (
         StatusCode::NOT_FOUND,
         layout.render(html! {
             h1 { "404 Page Not Found" }
-            p {"Original uri:" (original_uri.to_string())}
         }),
     )
 }
